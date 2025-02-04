4d8a1396a7bd00e2dfbc1c1afde9fd0f2204855e
jvictorxs <victor.victor19@hotmail.com>
2025-02-04 19:38:54 -0300

    conhecendo propriedades para javascript no html

--- dom1/script.js
+++ dom1/script.js
@@ -1,4 +1,12 @@
 const h1Selecionado = document.querySelector('h1');
 console.log(h1Selecionado)
 
-alert('hello guys')
+console.log(h1Selecionado.classList); //retorna as classes
+console.log(h1Selecionado.innerText); // retorna o texto
+console.log(h1Selecionado.id); // retorna o id
+console.log(h1Selecionado.offsetHeight); // retorna altura do elemento
+
+//h1Selecionado.addEventListener('click', callback);
+//ativa a função callback(comando de ação) ao click
+
+//alert('hello guys')
