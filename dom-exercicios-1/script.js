34feb7f815ea409eb7c6058b513f1cd3e8321423
jvictorxs <victor.victor19@hotmail.com>
2025-02-04 20:15:52 -0300

    Exercicio de fixacao acessando variaveis globais e propriedades

---
+++ dom-exercicios-1/script.js
@@ -0,0 +1,14 @@
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const classAtiva = document.querySelector('.ativo')
+console.log(classAtiva.innerText);
+console.log(classAtiva.className);
+
+// Retorne a linguagem do navegador
+console.log(window.navigator.language);
+console.log(window.navigator.languages[2]);
+// Retorne a largura da janela 
+console.log(window.innerWidth);
+
